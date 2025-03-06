9faccf39be34d5486562a9fd26b0c3f9ebecc119
Vriddhachalam <vriddhachalam.shanmugam@anjanetech.com>
2025-03-06 15:20:54 +0530

    added some files

--- github_events_dispatch.py
+++ github_events_dispatch.py
@@ -11,16 +11,10 @@ from typing import Any, Optional
 def repo_events(last_created_at=dlt.sources.incremental("created_at"),github_token: Optional[str] = dlt.secrets.value):
     url = "https://api.github.com/repos/Vriddhachalam-Anjane/dbt_self_for_django/events?per_page=100"
 
-
     while True:
         response = requests.get(url, auth=HTTPBasicAuth('username', github_token))
-
-
-        # response = requests.get(url)
         response.raise_for_status()
         yield response.json()
-
-
         # Stop requesting pages if the last element was already older than
         # the initial value.
         # Note: incremental will skip those items anyway, we just do not
@@ -28,13 +22,10 @@ def repo_events(last_created_at=dlt.sources.incremental("created_at"),github_tok
         if last_created_at.start_out_of_range:
             break
 
-
         # Get the next page.
         if "next" not in response.links:
             break
         url = response.links["next"]["url"]
-
-
 
 pipeline = dlt.pipeline(
     pipeline_name="github_events",
